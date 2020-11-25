d51e0d89414f7c8311d066e65f4103aea25e81b1
Derek Andersen <41839810+Dechrissen@users.noreply.github.com>
2020-11-25 14:00:48 -0500

    Add nginx_config

--- update_website.sh
+++ update_website.sh
@@ -4,11 +4,11 @@
 
 now=$(date +"%H-%M-%S_%m_%d_%Y")
 cd /home/derek/
-touch ./website_logs
+touch ./website.log
 cd /home/derek/derekandersen.net
 git checkout master -q
 rm -rf /var/www/derekandersennet
 rsync -avq /home/derek/derekandersen.net/ /var/www/derekandersennet --exclude .git --exclude .gitattributes --exclude README.md --exclude update_website.sh
-echo "$now updated" >> /home/derek/website_logs
+echo "$now updated" >> /home/derek/website.log
 sleep 1
 echo "derekandersen.net successfully updated!"
