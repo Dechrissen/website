1669c4e5858b72a45d1a8b2505a5f30f65f4061d
Derek Andersen <41839810+Dechrissen@users.noreply.github.com>
2020-11-25 14:07:52 -0500

    Update update_website.sh

--- update_website.sh
+++ update_website.sh
@@ -8,7 +8,7 @@ touch ./website.log
 cd /home/derek/derekandersen.net
 git checkout master -q
 rm -rf /var/www/derekandersennet
-rsync -avq /home/derek/derekandersen.net/ /var/www/derekandersennet --exclude .git --exclude .gitattributes --exclude README.md --exclude update_website.sh
+rsync -avq /home/derek/derekandersen.net/ /var/www/derekandersennet --exclude .git --exclude .gitattributes --exclude README.md --exclude update_website.sh --exclude nginx_config
 echo "$now updated" >> /home/derek/website.log
 sleep 1
 echo "derekandersen.net successfully updated!"
